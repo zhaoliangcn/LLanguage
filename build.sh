73adab2b920b9e9b5b8a88a35fb016dd8c91566e
zhaoliangcn <zhaoliangcn@126.com>
2022-07-03 10:43:13 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -4,15 +4,11 @@ make -C ./ScriptEngineLib
 make -C ./ScriptEngine
 rm -f ./ScriptEngineDll/dllmain.cpp
 make -C ./ScriptEngineDll
-#make -C ./QScriptEditor -f QMakefile
 make -C ./SocketAddin
-#make -C ./ZipAddin
 make -C ./OsInfoAddin
 mkdir -p bin/Addin
 cp ./ScriptEngine/scriptengine ./bin
 cp ./ScriptEngineDll/libScriptEngineDll.so ./bin
 cp ./SocketAddin/libsocketAddin.so ./bin/Addin
-#cp ./ZipAddin/libZipAddin.so ./bin/Addin
 cp ./OsInfoAddin/libosAddin.so ./bin/Addin
-#cp ./QScriptEditor/QScriptEditor ./bin
-#sudo update-mime-database /usr/share/mime
+
